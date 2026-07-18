de5ac5f29493702e09b5e33a1aba60eb86c19d6c
Luke Burton <lukesburton19@gmail.com>
2026-07-18 00:13:38 -0600

    nix: add amethyst mod manager flatpak

--- modules/nixos/desktop/gaming.nix
+++ modules/nixos/desktop/gaming.nix
@@ -6,4 +6,20 @@
     };
     gamemode.enable = true;
   };
+
+  services.flatpak.remotes = [
+    {
+      name = "amethyst";
+      location = "https://chrisdkn.github.io/Amethyst-Mod-Manager/amethyst.flatpakrepo";
+    }
+  ];
+
+  services.flatpak.packages = [
+    {
+      appId = "io.github.Amethyst.ModManager//stable";
+      origin = "amethyst";
+    }
+
+    "com.vysp3r.ProtonPlus"
+  ];
 }
