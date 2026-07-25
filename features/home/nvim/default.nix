4f2b5524d71ece5b35fbd0bad477d5af14844d75
Luke Burton <lukesburton19@gmail.com>
2026-07-25 01:05:48 -0600

    nvim proper setup

--- features/home/nvim/default.nix
+++ features/home/nvim/default.nix
@@ -1,16 +1,31 @@
-{ inputs, ... }: {
-  imports = [
-    inputs.nvf.homeManagerModules.default
-    ./opts.nix
-    ./autocmds.nix
-    ./plugins.nix
-  ];
-  programs.nvf = {
+{pkgs, ...}: {
+  programs.neovim = {
     enable = true;
-    settings.vim = {
-      viAlias = true;
-      vimAlias = true;
-    };
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    defaultEditor = true;
+
+    extraPackages = with pkgs; [
+      nil
+      alejandra
+      statix
+      stylua
+      lua-language-server
+    ];
+
+    plugins = with pkgs.vimPlugins; [
+      vague-nvim
+      mini-nvim
+      nvim-lspconfig
+      nvim-lint
+      fyler-nvim
+      conform-nvim
+      Navigator-nvim
+      gitsigns-nvim
+    ];
   };
-  home.sessionVariables.EDITOR = "nvim";
+
+  xdg.configFile."nvim/init.lua".source = ./init.lua;
 }
--- features/home/nvim/plugins.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }: {
-  programs.nvf.settings.vim = {
-    extraPlugins.tmux-navigator = {
-      package = pkgs.vimPlugins.vim-tmux-navigator;
-    };
-  };
-}
