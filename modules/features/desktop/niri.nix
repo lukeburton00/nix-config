57700433dac56e32d49fb28bceb01469c33d8de2
Luke Burton <lukesburton19@gmail.com>
2026-07-27 12:08:35 -0600

    add niri + noctalia via wrapper modules

---
+++ modules/features/desktop/niri.nix
@@ -0,0 +1,44 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.niri = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    programs.niri = {
+      enable = true;
+      package = self.packages.${pkgs.stdenv.hostPlatform.system}.niri;
+    };
+  };
+
+  perSystem = {
+    pkgs,
+    lib,
+    self',
+    ...
+  }: {
+    packages.niri = inputs.wrapper-modules.wrappers.niri.wrap {
+      inherit pkgs;
+      settings = {
+        spawn-at-startup = [
+          (lib.getExe self'.packages.noctalia)
+        ];
+
+        xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
+
+        input.keyboard.xkb.layout = "us,ua";
+
+        layout.gaps = 5;
+
+        binds = {
+          "Mod+Return".spawn-sh = "ghostty";
+          "Mod+Q".close-window = _: {};
+          "Mod+S".spawn-sh = "${lib.getExe self'.packages.noctalia} ipc call launcher toggle";
+        };
+      };
+    };
+  };
+}
---
+++ modules/features/desktop/noctalia.nix
@@ -0,0 +1,14 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  perSystem = {pkgs, ...}: {
+    packages.noctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
+      inherit pkgs;
+      # settings =
+      #   (builtins.fromJSON
+      #     (builtins.readFile ./noctalia.json)).settings;
+    };
+  };
+}
