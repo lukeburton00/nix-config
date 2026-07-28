705dde7d8b67aace83a7e1e001e5707cb4b25d97
Luke Burton <lukesburton19@gmail.com>
2026-07-28 14:46:08 -0600

    remove tmux from nix develop alias

--- modules/features/cli/zsh.nix
+++ modules/features/cli/zsh.nix
@@ -11,7 +11,7 @@
         v = "nvim";
         c = "clear";
         l = "ls -A -p --color=auto | sort";
-        nd = "nix develop --command zsh -c tmux";
+        nd = "nix develop --command zsh";
       };
       initContent = ''
         # Machine-local settings and secrets that should not be managed by Nix.
