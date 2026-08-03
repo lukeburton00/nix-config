9ae2e5e4a40d9a8ec2afd6d1ef2e5a289313c27b
Luke Burton <lukesburton19@gmail.com>
2026-08-03 16:37:57 -0600

    add zed and nh to home manager

--- modules/features/home-manager/default.nix
+++ modules/features/home-manager/default.nix
@@ -10,11 +10,15 @@
   }: {
     imports = [
       self.modules.homeManager.ghostty
+      self.modules.homeManager.zed
+      self.modules.homeManager.nh
       self.modules.homeManager.devtools
       self.modules.homeManager.worktools
     ];
 
     ghostty.enable = lib.mkDefault false;
+    zed.enable = lib.mkDefault false;
+    nh.enable = lib.mkDefault false;
     devtools.enable = lib.mkDefault false;
     worktools.enable = lib.mkDefault false;
   };
