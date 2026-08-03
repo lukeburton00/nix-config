c45a89262a536f66512993f4c3d31cc52869e6e2
Luke Burton <lukesburton19@gmail.com>
2026-08-03 14:56:45 -0600

    remove plymouth config

--- modules/features/nixos/base.nix
+++ modules/features/nixos/base.nix
@@ -46,11 +46,6 @@
         loader.systemd-boot.enable = true;
         loader.efi.canTouchEfiVariables = true;
         kernelPackages = pkgs.linuxPackages_latest;
-        kernelParams = [
-          "quiet"
-          "splash"
-        ];
-        plymouth.enable = true;
         kernel.sysctl = {
           "net.core.default_qdisc" = "fq";
           "net.ipv4.tcp_congestion_control" = "bbr";
