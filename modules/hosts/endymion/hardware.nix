862f0bbbbd2e55dac247d5463070ecd033424adc
Luke Burton <lukesburton19@gmail.com>
2026-08-04 23:19:33 -0600

    new uuids

--- modules/hosts/endymion/hardware.nix
+++ modules/hosts/endymion/hardware.nix
@@ -20,32 +20,32 @@
     boot.extraModulePackages = [];
 
     fileSystems."/" = {
-      device = "/dev/disk/by-uuid/a7a29beb-1cfd-496f-99ac-e53dd4ec8237";
+      device = "/dev/disk/by-uuid/3251db13-e56b-4628-876f-a62363c8fbc0";
       fsType = "btrfs";
     };
 
     fileSystems."/nix" = {
-      device = "/dev/disk/by-uuid/a7a29beb-1cfd-496f-99ac-e53dd4ec8237";
+      device = "/dev/disk/by-uuid/3251db13-e56b-4628-876f-a62363c8fbc0";
       fsType = "btrfs";
       options = ["subvol=nix"];
     };
 
     fileSystems."/boot" = {
-      device = "/dev/disk/by-uuid/9AC7-C193";
+      device = "/dev/disk/by-uuid/0EB0-F7E4";
       fsType = "vfat";
       options = ["fmask=0077" "dmask=0077"];
     };
 
     fileSystems."/home" = {
-      device = "/dev/disk/by-uuid/9cfe880e-7450-4cf4-b132-48a88e367d7b";
+      device = "/dev/disk/by-uuid/4b0eb9a5-1c6e-4ed1-abe8-210b63ca9757";
       fsType = "btrfs";
     };
 
     swapDevices = [
-      {device = "/dev/disk/by-uuid/0ea29636-7f7f-4b2e-a089-f0fc0cebd278";}
+      {device = "/dev/disk/by-uuid/fdfbbef1-958f-4fbd-a234-28cf29974b84";}
     ];
 
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    hardware.cpu.intel.updateMicrocode = true;
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
 }
